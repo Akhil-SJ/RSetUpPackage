e16f290c541b51275a5d6c3547151fe5f2877fdc
Akhil Jonnalagadda <60793135+Akhil-SJ@users.noreply.github.com>
2021-07-22 10:48:05 -0500

    Add files via upload

---
+++ AkhilsToolShed/R/getTools.R
@@ -0,0 +1,12 @@
+#' creates package library'
+#' @export
+#' @param x leave blank
+getTools = function(x) {
+
+  if (x == '')
+
+    install.packages(c('anytime',
+                     'bit'
+)    )
+
+}
