f2a24a6f8086f0b48dff1c8d9a8eb25bc8895d98
Akhil Jonnalagadda <60793135+Akhil-SJ@users.noreply.github.com>
2021-08-06 15:38:23 -0500

    Update AkhilsTest.R

--- AkhilsTest/R/AkhilsTest.R
+++ AkhilsTest/R/AkhilsTest.R
@@ -1,11 +1,79 @@
-prac <- function(df) {
+
+# fucntion e/ multple arguments
+  #defauts if missing values
+#optional arguments
 
 
-  install.packages(c('sjPlot','sjmisc','sjlabelled'))
+#option to run function if comannded to
 
-  library(sjPlot)
-  library(sjmisc)
-  library(sjlabelled)
+## return results and df, variable list
+
+## package speed
+
+?prac
+
+
+prac <- function(x,y = "defauly response"
+
+) {
+  ### check packages and call then. if do not exis they will download and from there it calls. if not found it lets you know
+
+  pkgTest <- function(x)
+  {
+    if (!require(x,character.only = TRUE))
+    {
+      install.packages(x,dep=TRUE)
+      if(!require(x,character.only = TRUE)) stop("Package not found")
+    }
+  }
+
+
+
+  pkgTest('sjPlot')
+
+  pkgTest('sjmisc')
+
+  pkgTest('sjlabelled')
+
+
+
+  raw_file = x
+
+
+  ### if soemthing exists function
+  check=function(x) tryCatch(if(class(x) == 'logical') 1 else 1, error=function(e) 0)
+
+
+
+
+
+  if((  check(y) == 1)
+
+
+  ) {
+    ext_in = y
+  } else {
+    ext_in = 2000
+  }
+
+
+
+
+
+
+
+
+
+
+  ### clean data
+
+
+  df = x[which(x[5]== 'setosa'),]
+  df = x[which(x[3] >= 1), ]
+
+
+  clean_data = df
+
 
   #df = rbind(x,y)
 
@@ -42,4 +110,23 @@ prac <- function(df) {
 
   tab_model(regmodel)
 
+
+
+
+
+  if( nchar(y) != 3
+
+
+  ) {
+    print(ext_in)
+  } else {
+    print('no')
+  }
+
+
+
+
+
+
 }
+
